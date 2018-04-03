aca4bfb92ac5c72815b97660e482354f78506c15
tom_ua <37613028+smai2018@users.noreply.github.com>
2018-04-03 16:59:01 +0200

    Create rap.control.sh

---
+++ rap.control.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+usage="usage message..."
+VERBOSE=true
+counter="0"
+while getopts 'edrsm?' option
+do
+  case "$option" in
+  e) systemctl start RAPTURE.service
+     systemctl status RAPTURE.service
+     ((counter+=1))
+     ;;
+  d) systemctl stop RAPTURE.service
+     systemctl status RAPTURE.service
+     ((counter+=1))
+     ;;
+  r) systemctl status RAPTURE.service
+     ((counter+=1))
+     ;;
+  s) /root/rapturecore-1.1.1/bin/rapture-cli mnsync status
+     ((counter+=1))
+     ;;
+  m) /root/rapturecore-1.1.1/bin/rapture-cli masternode status
+     ((counter+=1))
+     ;;
+  ?) $usage
+     exit 0
+     ;;
+  esac
+done
+if [ $counter -eq 1 ];then
+  exit 0
+else
+  echo $usage
+  exit 1
+fi
