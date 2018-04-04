af5acbce65f784f55bb5f1ecc67986fdac38b3dd
tom_ua <37613028+smai2018@users.noreply.github.com>
2018-04-04 15:04:37 +0200

    Update rap-control.sh

--- rap-control.sh
+++ rap-control.sh
@@ -1,37 +1,53 @@
 #!/bin/bash
 
-usage="usage message..."
-VERBOSE=true
-counter="0"
-while getopts 'edrsm?' option
-do
-  case "$option" in
-  e) systemctl start RAPTURE.service
-     systemctl status RAPTURE.service
-     ((counter+=1))
-     ;;
-  d) systemctl stop RAPTURE.service
-     systemctl status RAPTURE.service
-     ((counter+=1))
-     ;;
-  r) systemctl is-active RAPTURE.service
-     systemctl is-enabled RAPTURE.service
-     ((counter+=1))
-     ;;
-  s) /root/rapturecore-1.1.1/bin/rapture-cli mnsync status
-     ((counter+=1))
-     ;;
-  m) /root/rapturecore-1.1.1/bin/rapture-cli masternode status
-     ((counter+=1))
-     ;;
-  ?) $usage
-     exit 0
-     ;;
-  esac
-done
+if [[ $USER != "root" ]]; then 
+		echo "This script must be run as root!" 
+		exit 1
+	fi
 if [ $counter -eq 1 ];then
   exit 0
 else
   echo $usage
   exit 1
 fi
+#usage="./rap-control.sh -a"
+usage echo -e "\nUsage:\n__g5_token5ac4cc643c0ac [arguments] \n"
+VERBOSE=true
+counter="0"
+
+while getopts 'abcdefghi?' option
+do
+  case "$option" in
+  a) systemctl start RAPTURE
+     systemctl is-active RAPTURE
+     ((counter+=1))
+     ;;
+  b) systemctl stop RAPTURE
+     systemctl is-active RAPTURE
+     ((counter+=1))
+     ;;
+  c) systemctl status RAPTURE
+     ((counter+=1))
+     ;;
+  d) systemctl is-enabled RAPTURE.service
+     ;;
+  e) /root/rapturecore-1.1.1/bin/rapture-cli mnsync status
+     ((counter+=1))
+     ;;
+  f) /root/rapturecore-1.1.1/bin/rapture-cli masternode status
+     ((counter+=1))
+     ;;
+  g) cat /root/sentinel/sentinel.log
+     ((counter+=1))
+     ;;
+  h) $usage
+     ((counter+=1))
+     ;;   
+  i) grep CRON /var/log/syslog
+     ((counter+=1))
+     ;;   
+  ?) $usage
+     exit 0
+     ;;
+  esac
+done
